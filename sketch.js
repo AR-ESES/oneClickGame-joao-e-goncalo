5fac49f0b6df43a1de85b461f304d3dd7c50f942
GoncaloPeseiro <102664610+GoncaloPeseiro@users.noreply.github.com>
2022-06-03 13:52:38 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,29 @@
+var xLoc = 0;
+var yLoc = 0;
+var score = 0;
+var speed = 1;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
+  createCanvas(400, 400);
 }
 
 function draw() {
-	// put drawing code here
+  background(220);
+  frameRate(speed);
+  xLoc = random(0, width - 50);
+  yLoc = random(0, height - 50);
+  text("Score: " + score, 20, 21);
+  
+  rect(xLoc, yLoc, 50, 50);
+}
+
+function mousePressed() {
+  if (mouseX > xLoc && mouseX < xLoc + 50 && mouseY > yLoc && mouseY < yLoc + 50) {
+    // console.log("button is clicked!")
+    score = score + 1;
+    instructions = "";
+  } else {
+    // console.log("you missed the button")
+    score = 0;
+  }
 }
